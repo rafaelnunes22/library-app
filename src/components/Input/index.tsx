e2846855906231a9bce5eeefc7eaf9705a266297
unknown <rafael.nunes@meta.com.br>
2021-11-26 14:03:35 -0300

    create component Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,15 @@
+import { Fragment, InputHTMLAttributes, ReactElement } from "react";
+import { Wrapper, Input, Hr } from "./styles";
+
+type Props = InputHTMLAttributes<HTMLInputElement>;
+
+export function InputText(props: Props) {
+  return (
+    <Wrapper>
+      <Input {...props} />
+      <Hr />
+    </Wrapper>
+  )
+}
+
+export { InputText as Input };
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+`;
+
+export const Input = styled.input`
+  font-size: 20px;
+  background-color: transparent;
+  color: white;
+
+  width: 100%;
+
+  border: none;
+  outline: none;
+
+  
+`;
+
+export const Hr = styled.hr`
+  background-color: white;
+
+  height: 1px;
+  width: 100%;
+
+  border: none;
+  border-radius: 10px;
+`;
